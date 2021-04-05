8b6f522bd0e95aaf67abcece313d43a54812da6c
Sean <skabe89@gmail.com>
2021-04-05 17:02:52 -0400

    started room

---
+++ src/room/Room.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './Room.css'
+import Tile from './Tile'
+
+export default class Room extends Component {
+  render() {
+    return (
+      <div>
+        <Tile />
+      </div>
+    )
+  }
+}
---
+++ src/room/Tile.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Room.css'
+
+export default function Tile() {
+  return (
+    <div>
+      Hello this is a tile
+    </div>
+  )
+}
