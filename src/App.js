b003a2417ccac27e4d901946cd91249498cc28c2
Sean <skabe89@gmail.com>
2021-04-05 17:29:04 -0400

    added basic routing functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 
 import './App.css';
-import Room from './room/Room'
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import Room from './room/Room'
+import Home from  './Home'
 
 function App() {
   return (
-    <div className="App">
-      <Room />
+    <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/room">Room</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/room">
+          <Room />
+        </Route>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
